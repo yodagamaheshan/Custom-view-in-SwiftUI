a580a555dd16e1001080f24a09c0fc109ef12530
Heshan Yodagama <yodagamaheshan@gmail.com>
2020-02-12 06:53:44 +0530

    Initial Commit

---
+++ Custom View with SwiftUI WWDC 2019/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Custom View with SwiftUI WWDC 2019
+//
+//  Created by Heshan Yodagama on 2/12/20.
+//  Copyright © 2020 Heshan Yodagama. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
